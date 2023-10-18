acea45283855103499db742e99d921cf0b2a8af2
Therealaly <aadley85@gmail.com>
2023-10-18 10:53:57 +0700

    Api Integration and display

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  KEY: '7a0df20282de926526348771b49a9552',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+};
+
+export default CONFIG;
